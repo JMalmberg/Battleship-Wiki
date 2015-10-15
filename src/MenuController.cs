5c426bbb411fa9d69ddc5e7248064e21d3ff3778
sdawson12 <100598945@student.swin.edu.au>
2015-10-16 02:33:00 +1100

    changed setup to difficulty and font to white

--- src/MenuController.cs
+++ src/MenuController.cs
@@ -28,7 +28,7 @@ static class MenuController
 	private static readonly string[][] _menuStructure = {
 		new string[] {
 			"PLAY",
-			"SETUP",
+			"DIFFICULTY",
 			"SCORES",
 			"QUIT"
 		},
@@ -70,9 +70,9 @@ static class MenuController
 	private const int GAME_MENU_SURRENDER_BUTTON = 1;
 
 	private const int GAME_MENU_QUIT_BUTTON = 2;
-	private static readonly Color MENU_COLOR = SwinGame.RGBAColor(2, 167, 252, 255);
+	private static readonly Color MENU_COLOR = SwinGame.RGBAColor(255, 255, 255, 255);
 
-	private static readonly Color HIGHLIGHT_COLOR = SwinGame.RGBAColor(1, 57, 86, 255);
+	private static readonly Color HIGHLIGHT_COLOR = SwinGame.RGBAColor(255, 255, 255, 255);
 	/// <summary>
 	/// Handles the processing of user input when the main menu is showing
 	/// </summary>
